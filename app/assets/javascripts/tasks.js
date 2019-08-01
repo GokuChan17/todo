15e2aaaac34ee4d41abd1eff54a6054c19e7d1dc
Sonia <vsoniahuoy@gmail.com>
2019-08-01 23:26:26 +0000

    Add crossthrough todos item

--- app/assets/javascripts/tasks.js
+++ app/assets/javascripts/tasks.js
@@ -6,14 +6,16 @@
     // <li> tags
     function taskHtml(task) {
       var checkedStatus = task.done ? "checked" : "";
-      var liElement = '<li><div class="view"><input class="toggle" type="checkbox"' +
+      var liClass = task.done ? "completed" : "";
+      var liElement = '<li id="listItem-' + task.id +'"class="' + liClass + '">' +
+      '<div class="view"><input class="toggle" type="checkbox"' +
         " data-id='" + task.id + "'" +
         checkedStatus +
         '><label>' +
          task.title +
          '</label></div></li>';
 
-      return liElement;
+    return liElement;
     }
 
     // toggleTask takes in an HTML representation of
@@ -32,6 +34,12 @@
         task: {
           done: doneValue
         }
+      }).success(function(data) {
+        var liHtml = taskHtml(data);
+        var $li = $("#listItem-" + data.id);
+        $li.replaceWith(liHtml)
+        // event handler to change 
+        $('.toggle').change(toggleTask);
       });
     }
 
